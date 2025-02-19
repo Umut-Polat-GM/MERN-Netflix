b9f39887ebde106663273420041dc78295e84b16
Umut-Polat-GM <umut.polat.272@gmail.com>
2025-02-19 17:00:16 +0300

    add movie controller and routes for fetching trending movies, details, trailers, and similar movies

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ import express from "express";
 import dotenv from "dotenv";
 
 import authRoutes from "./routes/auth.routes.js";
+import movieRoutes from "./routes/movie.routes.js";
 import { connectDB } from "./lib/db.js";
 
 dotenv.config();
@@ -13,6 +14,7 @@ const PORT = process.env.PORT || 5000;
 app.use(express.json()); // will allow us to parse req.body
 
 app.use("/api/auth", authRoutes);
+app.use("/api/movie", movieRoutes);
 
 app.listen(PORT, () => {
     console.log("Server is running on port http://localhost:" + PORT);
