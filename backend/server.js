692b22af2ff49a3f260338bb47b447bdab53a3c5
Umut-Polat-GM <umut.polat.272@gmail.com>
2025-02-20 14:04:26 +0300

    add TV controller and routes for fetching trending shows, details, trailers, and similar series

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,8 @@ import dotenv from "dotenv";
 
 import authRoutes from "./routes/auth.routes.js";
 import movieRoutes from "./routes/movie.routes.js";
+import tvRoutes from "./routes/tv.routes.js";
+
 import { connectDB } from "./lib/db.js";
 
 dotenv.config();
@@ -15,6 +17,7 @@ app.use(express.json()); // will allow us to parse req.body
 
 app.use("/api/auth", authRoutes);
 app.use("/api/movie", movieRoutes);
+app.use("/api/tv", tvRoutes);
 
 app.listen(PORT, () => {
     console.log("Server is running on port http://localhost:" + PORT);
