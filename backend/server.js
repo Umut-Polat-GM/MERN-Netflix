6c83be18f158d81c84ff669e04fdc59b7c29464c
Umut-Polat-GM <umut.polat.272@gmail.com>
2025-02-20 16:45:25 +0300

    add search functionality with controllers and routes for movies, TV shows, and search history management

--- backend/server.js
+++ backend/server.js
@@ -6,6 +6,7 @@ import cookieParser from "cookie-parser";
 import authRoutes from "./routes/auth.routes.js";
 import movieRoutes from "./routes/movie.routes.js";
 import tvRoutes from "./routes/tv.routes.js";
+import searchRoutes from "./routes/search.routes.js";
 
 import { connectDB } from "./lib/db.js";
 import { protectRoute } from "./middleware/protectRoute.js";
@@ -22,6 +23,7 @@ app.use(cookieParser()); // will allow us to parse cookies for req.cookies prote
 app.use("/api/auth", authRoutes);
 app.use("/api/movie", protectRoute, movieRoutes);
 app.use("/api/tv", protectRoute, tvRoutes);
+app.use("/api/search", protectRoute, searchRoutes);
 
 app.listen(PORT, () => {
     console.log("Server is running on port http://localhost:" + PORT);
